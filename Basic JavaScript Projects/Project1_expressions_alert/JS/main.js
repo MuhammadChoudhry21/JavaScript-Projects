848c27d054e3e8c94c5c43a68959b7105bb1ed84
MuhammadChoudhry21 <fazal.zoom@gmail.com>
2023-06-12 10:39:17 -0400

    Pushing the 'Basic JavaScript Projects Assignment' to github

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,39 @@
+//Alert message using windows.alert:
+window.alert("This is an alert message made with the alert method in Js!");
+
+//Text display using document.write:
+document.write("This text was made using the document.write method!");
+
+//String Value in a variable:
+var exampleVariable = ("<br>This is a string value in a variable using Js!");
+document.write(exampleVariable);
+
+//String value variable and using the window.alert message:
+var exampleVariableAlert = ("This is a string value variable made with the window.alert method!");
+window.alert(exampleVariableAlert);
+
+//Escape character:
+var escapeCharacter = ("Bob said:<br> \"I love coding\"");
+document.write(escapeCharacter);
+
+//Displaying a concatenated text:
+document.write("<br>\"Think only of" + " your art.\"");
+
+//concatenated string value to a variable:
+var B = "Concatenated" + "String"
+document.write(B);
+
+//Multiple variables assignment and displaying one of them:
+var Family = "The Wests", Dad = "Bob", Mom = "Sarah", Son = "George", Daughter = "Tracy";
+document.write(Mom)
+
+//Expression:
+document.write(2 + 2);
+
+//Remaining Parts of The 'Basic JavaScript Projects Assignment' Section
+
+//Creating 2 variables sent1 & sent2:
+
+var Sent1 = "<br>This is the beginning of the string", Sent2 = "<br>This is the end of the string";
+document.write(Sent1 + Sent2);
+
